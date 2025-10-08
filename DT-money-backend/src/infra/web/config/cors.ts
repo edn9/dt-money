0d1d45d0ffc53b25f8d0e8b148a0dd61a4a921bc
Ed. Brito <eduardohenriquen9@outlook.com>
2025-10-08 13:14:07 -0300

    Minor edits

--- DT-money-backend/src/infra/web/config/cors.ts
+++ DT-money-backend/src/infra/web/config/cors.ts
@@ -2,5 +2,7 @@ import { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
 
 export const register = async (fastify: FastifyInstance) => {
-  await fastify.register(cors);
+  await fastify.register(cors, {
+    //    origin: true,
+  });
 };
